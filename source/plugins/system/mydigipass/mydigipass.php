856724e6f915d3398a48137a8774b6ec6cc60ac4
Daniel Dimitrov <daniel@compojoom.com>
2014-06-21 23:51:19 +0200

    mergin the plugins and code cleanup

--- source/plugins/system/mydigipass/mydigipass.php
+++ source/plugins/system/mydigipass/mydigipass.php
@@ -27,11 +27,99 @@ class PlgSystemMydigipass extends JPlugin
 		$input = JFactory::getApplication()->input;
 
 		// Check if mydigipass get parameter is set
-		if (!$input->get('mydigipass', 0))
+		if ($input->get('mydigipass', 0))
+		{
+			$this->frontend();
+			return true;
+		}
+
+		// Check if mydigipass get parameter is set
+		if ($input->getInt('digiadmin', 0))
+		{
+			$this->backend();
+			return true;
+		}
+
+		return true;
+
+	}
+
+	/**
+	 * Handle the backend login logic
+	 *
+	 * @return bool
+	 * @throws Exception
+	 */
+	private function backend() {
+		$input = JFactory::getApplication()->input;
+		$uuid = $input->get('uuid', 0);
+
+		// Check if uuid is not empty
+		if (!$uuid)
 		{
 			return true;
 		}
 
+		$user = JFactory::getUser();
+
+		// We don't do anything if we are already logged in the backend
+		if ($user->id)
+		{
+			return true;
+		}
+
+		$inputCookie  = JFactory::getApplication()->input->cookie;
+		$digisession = $inputCookie->get("digipasssession", 0);
+		$token = $inputCookie->get('digitoken', 0);
+
+		$session = JFactory::getSession();
+
+		// Check session
+		if ($session->getId() != $digisession)
+		{
+			return true;
+		}
+
+		// Check token
+		if (empty($token))
+		{
+			return true;
+		}
+
+		// Let's check if the token matches the saved one in the database
+		$result = $this->getProfileRow($token);
+
+		if (empty($result))
+		{
+			throw new Exception('The security token does not match!');
+		}
+
+		$this->deleteToken($token);
+
+		// Unset cookies
+		$inputCookie->set('digipassadmin', "", time() -1, "/");
+		$inputCookie->set('digipasssession', "", time() -1, "/");
+		$inputCookie->set('digitoken', "", time() -1, "/");
+
+		$user = $this->getUser($uuid);
+
+		$this->login($user, array('action' => 'core.login.admin'));
+
+		JFactory::getApplication()->redirect(JUri::base() . "index.php");
+
+		return true;
+	}
+
+	/**
+	 * Handle the login logic in the frontend.
+	 * If user is an admin that tries to login from the backend, redirects to the backend
+	 * so that we can log him there
+	 *
+	 * @return bool
+	 * @throws Exception
+	 */
+	private function frontend() {
+		$input = JFactory::getApplication()->input;
 		$code = $input->get('code', 0);
 
 		// Check if the oauth code is supplied
@@ -40,17 +128,23 @@ class PlgSystemMydigipass extends JPlugin
 			return true;
 		}
 
-		// Check if we connect or login
-		$user = JFactory::getUser();
-
+		$user        = JFactory::getUser();
 		$inputCookie = JFactory::getApplication()->input->cookie;
 		$isAdmin     = $inputCookie->get("digipassadmin", 0);
 
 		// Fix
-		$mdp_base_uri  = ($this->params->get("sandbox", 0)) ? "https://sandbox.mydigipass.com" : "https://mydigipass.com";
+		$mdp_base_uri  = $this->params->get("sandbox", 0) ? "https://sandbox.mydigipass.com" : "https://mydigipass.com";
 		$client_id     = $this->params->get("clientid", "");
 		$client_secret = $this->params->get("clientsecret", "");
 		$redirect_uri  = $this->params->get("redirecturi", "");
+
+		// Login request
+		$options = new JRegistry();
+		$options->set("redirecturi", $redirect_uri);
+		$options->set("clientid", $client_id);
+		$options->set("clientsecret", $client_secret);
+		$options->set("tokenurl", $mdp_base_uri . "/oauth/token");
+		$service = new JOAuth2Client($options);
 
 		// Check if should connect - on admin we always login, even if the user is logged into the frontend
 		if ($user->id > 0 && !$isAdmin)
@@ -63,15 +157,6 @@ class PlgSystemMydigipass extends JPlugin
 			$session->set('client_id', $client_id);
 			$session->set('client_secret', $client_secret);
 
-			$options = new JRegistry();
-
-			$options->set("redirecturi", $redirect_uri);
-			$options->set("clientid", $client_id);
-			$options->set("clientsecret", $client_secret);
-			$options->set("tokenurl", $mdp_base_uri . "/oauth/token");
-
-			$service = new JOAuth2Client($options);
-
 			try
 			{
 				$return = $service->authenticate();
@@ -81,24 +166,8 @@ class PlgSystemMydigipass extends JPlugin
 					throw new RuntimeException("Error connecting profile");
 				}
 
-				$uuid = $return["uuid"];
-
-				// Save the uuid in the profile database
-				$db    = JFactory::getDbo();
-				$query = "INSERT INTO #__user_profiles VALUES(
-								" . $db->quote($user->id) . ",
-								" . $db->quote("uuid") . ",
-								" . $db->quote($uuid) . ",
-								0
-							)";
-
-				$db->setQuery($query);
-				$db->execute();
-
-				if ($db->getErrorNum())
-				{
-					throw new Exception("Error saving uuid " . $db->getErrorMsg());
-				}
+				// Save the uuid in the database
+				$this->insertIntoProfile($user->id, 'uuid', $return['uuid']);
 			}
 			catch (Exception $e)
 			{
@@ -109,93 +178,18 @@ class PlgSystemMydigipass extends JPlugin
 		{
 			try
 			{
-				// Login request
-				$options = new JRegistry();
-
-				$options->set("redirecturi", $redirect_uri);
-				$options->set("clientid", $client_id);
-				$options->set("clientsecret", $client_secret);
-				$options->set("tokenurl", $mdp_base_uri . "/oauth/token");
-
-				$service = new JOAuth2Client($options);
 				$return = $service->authenticate();
-
-				$uuid = $return["uuid"];
 
 				if (empty($return))
 				{
 					throw new RuntimeException("Error loging in");
 				}
 
-				// Let's check if the user is already connected
-				$db = JFactory::getDbo();
-
-				$db->setQuery(
-					'SELECT * FROM #__user_profiles WHERE profile_value = ' . $db->quote($uuid)
-				);
-
-				$result = $db->loadObject();
-
-				if (empty($result))
-				{
-					// Show connect button only if not connected yet
-					throw new Exception('You are not connected with this Joomla site!');
-				}
-
-				$user = JFactory::getUser($result->user_id);
-
-				if (empty($user) || $user->block)
-				{
-					throw new Exception('Your user account was not found!');
-				}
+				$user = $this->getUser($return["uuid"]);
 
 				if (!$isAdmin)
 				{
-					// Login User
-					// Get the global JAuthentication object.
-					jimport('joomla.user.authentication');
-
-					// We need that for the auth response to work! (Don't delete)
-					$authenticate = JAuthentication::getInstance();
-
-					// Get plugins
-					$plugins = JPluginHelper::getPlugin('authentication');
-
-					// Create authentication response
-					$response = new JAuthenticationResponse;
-
-					$response->username = $user->username;
-					$response->fullname = $user->name;
-					$response->password = $user->password;
-					$response->status   = JAuthentication::STATUS_SUCCESS;
-
-					$opt = array();
-
-					$opt['action'] = "core.login";
-
-					if ($isAdmin)
-					{
-						$opt['action']   = "core.login.admin";
-						$opt['clientid'] = 1;
-					}
-
-					// Import the user plugin group.
-					JPluginHelper::importPlugin('user');
-					$dispatcher = JEventDispatcher::getInstance();
-
-					// OK, the credentials are authenticated and user is authorised.  Let's fire the onLogin event.
-					$results = $dispatcher->trigger('onUserLogin', array((array) $response, $opt));
-
-					$user = JFactory::getUser();
-
-					$user->set('cookieLogin', true);
-
-					$opt['user']         = $user;
-					$opt['responseType'] = $response->type;
-
-					// The user is successfully logged in. Run the after login events
-					$dispatcher->trigger('onUserAfterLogin', array($opt));
-
+					$this->login($user, array('action' => 'core.login'));
 				}
 				else
 				{
@@ -203,27 +197,16 @@ class PlgSystemMydigipass extends JPlugin
 					// Generate token for security
 					$token = JSession::getFormToken(true);
 
+					// Make sure that we don't have the same token
+					$this->deleteToken($token);
+
 					// Save the token in the profile database
-					$db    = JFactory::getDbo();
-					$query = "INSERT INTO #__user_profiles VALUES(
-								" . $db->quote($user->id) . ",
-								" . $db->quote("token") . ",
-								" . $db->quote($token) . ",
-								0
-							)";
-
-					$db->setQuery($query);
-					$db->execute();
-
-					// TODO Exception handling..
-					if ($db->getErrorNum())
-					{
-						throw new Exception("Error saving token " . $db->getErrorMsg());
-					}
+					$this->insertIntoProfile($user->id, 'token', $token);
 
 					$inputCookie->set('digitoken', $token, time() + 120, "/");
 
-					return JFactory::getApplication()->redirect(JUri::base() . "administrator/index.php?digiadmin=1&uuid=" . $uuid);
+					JFactory::getApplication()->redirect(JUri::base() . "administrator/index.php?digiadmin=1&uuid=" . $return["uuid"]);
+					return true;
 				}
 			}
 			catch (Exception $e)
@@ -231,6 +214,132 @@ class PlgSystemMydigipass extends JPlugin
 				echo 'Exception caught: ' . $e->getMessage();
 			}
 		}
+
+		return true;
+	}
+
+	/**
+	 * Inserts a row in the user_profile table
+	 *
+	 * @param   int     $id     - the user id
+	 * @param   string  $key    - the key
+	 * @param   string  $value  - the value to store
+	 */
+	private function insertIntoProfile($id, $key, $value) {
+		// Save the row in the profile database
+		$db    = JFactory::getDbo();
+		$query = "INSERT INTO #__user_profiles VALUES(
+								" . $db->quote($id) . ",
+								" . $db->quote($key) . ",
+								" . $db->quote($value) . ",
+								0
+							)";
+
+		$db->setQuery($query);
+
+		try
+		{
+			$db->execute();
+		}
+		catch (Exception $e)
+		{
+			echo "Error saving " . $key . ':' . $e->getMessage();
+		}
+	}
+
+	/**
+	 * Gets a joomla user out of the UUID
+	 *
+	 * @param   string  $uuid  - Unique user identifier
+	 *
+	 * @return JUser
+	 * @throws Exception
+	 */
+	private function getUser($uuid) {
+		$result = $this->getProfileRow($uuid);
+
+		if (empty($result))
+		{
+			// Show connect button only if not connected yet
+			throw new Exception('You are not connected with this Joomla site!');
+		}
+
+		$user = JFactory::getUser($result->user_id);
+
+		if (empty($user) || $user->block)
+		{
+			throw new Exception('Your user account was not found!');
+		}
+
+		return $user;
+	}
+
+	/**
+	 * Delete a token out of the database
+	 *
+	 * @param   string  $token  - the security token that we've saved
+	 */
+	private function deleteToken($token) {
+		$db = JFactory::getDbo();
+		// Delete the token (it should just be used once!)
+		$db->setQuery("DELETE FROM #__user_profiles WHERE profile_value = " . $db->quote($token));
+		$db->execute();
+	}
+
+	/**
+	 * Get a row matchin the profile value
+	 *
+	 * @param   string  $value  - the value to look for
+	 *
+	 * @return mixed
+	 */
+	private function getProfileRow($value) {
+		// Let's check if the user is already connected
+		$db = JFactory::getDbo();
+
+		$db->setQuery(
+			'SELECT * FROM #__user_profiles WHERE profile_value = ' . $db->quote($value)
+		);
+
+		$result = $db->loadObject();
+
+		return $result;
+	}
+
+	private function login($user, $opt = array()){
+		// Login User
+		// Get the global JAuthentication object.
+		jimport('joomla.user.authentication');
+
+		JAuthentication::getInstance();
+
+		// Get plugins
+		JPluginHelper::getPlugin('authentication');
+
+		// Create authentication response
+		$response = new JAuthenticationResponse;
+
+		$response->username = $user->username;
+		$response->fullname = $user->name;
+		$response->password = $user->password;
+		$response->status = JAuthentication::STATUS_SUCCESS;
+
+		// Import the user plugin group.
+		JPluginHelper::importPlugin('user');
+		$dispatcher = JEventDispatcher::getInstance();
+
+		// OK, the credentials are authenticated and user is authorised.  Let's fire the onLogin event.
+		$dispatcher->trigger('onUserLogin', array((array) $response, $opt));
+
+		$user = JFactory::getUser();
+
+		$user->set('cookieLogin', true);
+
+		$opt['user'] = $user;
+		$opt['responseType'] = $response->type;
+
+		// The user is successfully logged in. Run the after login events
+		$dispatcher->trigger('onUserAfterLogin', array($opt));
 	}
 
 	/**
